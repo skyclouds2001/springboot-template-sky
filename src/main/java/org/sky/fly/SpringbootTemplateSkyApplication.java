6b13cc89aecef0d6a953af24dc28c21f3c7b41d9
skyclouds2001 <skyclouds2001@163.com>
2023-04-11 12:25:50 +0800

    feat: init project

---
+++ src/main/java/org/sky/fly/SpringbootTemplateSkyApplication.java
@@ -0,0 +1,13 @@
+package org.sky.fly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTemplateSkyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootTemplateSkyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sky/fly/SpringbootTemplateSkyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sky.fly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTemplateSkyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
