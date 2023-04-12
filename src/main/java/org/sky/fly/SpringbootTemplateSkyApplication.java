f82c792d0cb5cab8a8d8576f43ed4b77a18795fc
skyclouds2001 <skyclouds2001@163.com>
2023-04-12 17:26:43 +0800

    feat: remove MapperScan from main class to config class

--- src/main/java/org/sky/fly/SpringbootTemplateSkyApplication.java
+++ src/main/java/org/sky/fly/SpringbootTemplateSkyApplication.java
@@ -1,11 +1,9 @@
 package org.sky.fly;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("org.sky.fly.mapper")
 public class SpringbootTemplateSkyApplication {
 
     public static void main(String[] args) {
