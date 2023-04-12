27f09c0133308bfb84ba9aae9b7ad5756a5ef220
skyclouds2001 <skyclouds2001@163.com>
2023-04-12 17:23:24 +0800

    feat: update city controller

--- src/main/java/org/sky/fly/controller/CityController.java
+++ src/main/java/org/sky/fly/controller/CityController.java
@@ -1,24 +1,50 @@
 package org.sky.fly.controller;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.sky.fly.model.City;
 import org.sky.fly.service.CityService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/city")
+@ResponseBody
 public class CityController {
 
     @Autowired
     private CityService cityService;
 
-    @GetMapping("/")
+    @GetMapping("/{id}")
+    public City getCity(@PathVariable Integer id) {
+        return cityService.getById(id);
+    }
+
+    @GetMapping
+    public Page<City> getCityList(@RequestParam Integer page, @RequestParam Integer size) {
+        return cityService.page(new Page<>(page, size));
+    }
+
+    @GetMapping("/all")
     public List<City> getCityList() {
         return cityService.list();
+    }
+
+    @PostMapping
+    public void addCity(@RequestBody City city) {
+        cityService.save(city);
+    }
+
+    @PutMapping
+    public void updateCity(@RequestBody City city) {
+        cityService.updateById(city);
+    }
+
+    @DeleteMapping("/{id}")
+    public void removeCity(@PathVariable Integer id) {
+        cityService.removeById(id);
     }
 
 }
