fc60fd50de8cf294f5c29610a00befde75032b98
skyclouds2001 <skyclouds2001@163.com>
2023-04-12 17:03:51 +0800

    feat: add Serial for city model

--- src/main/java/org/sky/fly/model/City.java
+++ src/main/java/org/sky/fly/model/City.java
@@ -5,9 +5,15 @@ import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
+import java.io.Serial;
+import java.io.Serializable;
+
 @Data
 @TableName("city")
-public class City {
+public class City implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
 
     @TableId("id")
     Integer id;
