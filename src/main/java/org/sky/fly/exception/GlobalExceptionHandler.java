e045e1d9bf40d6aca74dd8405f54dc69d954aa0a
skyclouds2001 <skyclouds2001@163.com>
2023-04-18 13:20:55 +0800

    feat: use logger in exception handler

--- src/main/java/org/sky/fly/exception/GlobalExceptionHandler.java
+++ src/main/java/org/sky/fly/exception/GlobalExceptionHandler.java
@@ -1,24 +1,26 @@
 package org.sky.fly.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.sky.fly.result.Result;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     @ResponseBody
     public Result<?> defaultExceptionHandler(Exception e) {
-        e.printStackTrace();
+        log.error("[Exception]", e);
         return Result.fail();
     }
 
     @ExceptionHandler(CustomException.class)
     @ResponseBody
     public Result<?> customExceptionHandler(CustomException e) {
-        e.printStackTrace();
+        log.error("[Custom Exception]", e);
         return Result.build(null, e.getCode());
     }
 
