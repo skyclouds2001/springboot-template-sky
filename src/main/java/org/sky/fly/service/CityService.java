322885c96c45079d86781f8b8e245d53d97923ef
skyclouds2001 <skyclouds2001@163.com>
2023-04-18 13:30:22 +0800

    feat: add transition support

--- src/main/java/org/sky/fly/service/CityService.java
+++ src/main/java/org/sky/fly/service/CityService.java
@@ -3,6 +3,8 @@ package org.sky.fly.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import org.sky.fly.model.City;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 public interface CityService extends IService<City> {
 }
