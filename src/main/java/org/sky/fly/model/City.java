6668459edccf33acf99bfc1a7f2921785870c4b4
skyclouds2001 <skyclouds2001@163.com>
2023-04-19 01:30:59 +0800

    feat: update city model with additional lombok method

--- src/main/java/org/sky/fly/model/City.java
+++ src/main/java/org/sky/fly/model/City.java
@@ -2,14 +2,18 @@ package org.sky.fly.model;
 
 import com.baomidou.mybatisplus.annotation.*;
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serial;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @Schema(description = "城市")
+@AllArgsConstructor
 @Data
+@NoArgsConstructor
 @TableName("city")
 public class City implements Serializable {
 
