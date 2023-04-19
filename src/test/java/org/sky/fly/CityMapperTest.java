30854574db1ea1a20103738cf6eb00fcdd8b9550
skyclouds2001 <skyclouds2001@163.com>
2023-04-19 20:46:16 +0800

    test: add city mapper test

--- src/test/java/org/sky/fly/CityMapperTest.java
+++ src/test/java/org/sky/fly/CityMapperTest.java
@@ -15,9 +15,44 @@ class CityMapperTest {
     CityMapper cityMapper;
 
     @Test
+    void testSelect() {
+        City city = cityMapper.selectById(0);
+        System.out.println(city);
+    }
+
+    @Test
     void testSelectAll() {
         List<City> list = cityMapper.selectList(null);
         list.forEach(System.out::println);
+    }
+
+    @Test
+    void testInsert() {
+        City city = new City();
+        city.setName("test city");
+        city.setDistrict("test district");
+        city.setCountry("test country");
+        city.setPopulation(1000);
+        int result = cityMapper.insert(city);
+        System.out.println(result);
+    }
+
+    @Test
+    void testUpdate() {
+        City city = new City();
+        city.setId(0);
+        city.setName("test city");
+        city.setDistrict("test district");
+        city.setCountry("test country");
+        city.setPopulation(1000);
+        int result = cityMapper.updateById(city);
+        System.out.println(result);
+    }
+
+    @Test
+    void testRemove() {
+        int result = cityMapper.deleteById(0);
+        System.out.println(result);
     }
 
 }
