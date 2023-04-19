a0ece9bf6339f3175cafea3d6794e3833061fd3d
skyclouds2001 <skyclouds2001@163.com>
2023-04-19 20:46:27 +0800

    test: add city service test

--- src/test/java/org/sky/fly/CityServiceTest.java
+++ src/test/java/org/sky/fly/CityServiceTest.java
@@ -15,9 +15,45 @@ public class CityServiceTest {
     CityService cityService;
 
     @Test
+    void testSelect() {
+        City city = cityService.getById(0);
+        System.out.println(city);
+    }
+
+    @Test
     void testGetList() {
         List<City> list = cityService.list();
         list.forEach(System.out::println);
     }
+
+    @Test
+    void testInsert() {
+        City city = new City();
+        city.setName("test city");
+        city.setDistrict("test district");
+        city.setCountry("test country");
+        city.setPopulation(1000);
+        boolean result = cityService.save(city);
+        System.out.println(result);
+    }
+
+    @Test
+    void testUpdate() {
+        City city = new City();
+        city.setId(0);
+        city.setName("test city");
+        city.setDistrict("test district");
+        city.setCountry("test country");
+        city.setPopulation(1000);
+        boolean result = cityService.updateById(city);
+        System.out.println(result);
+    }
+
+    @Test
+    void testRemove() {
+        boolean result = cityService.removeById(0);
+        System.out.println(result);
+    }
+
 
 }
