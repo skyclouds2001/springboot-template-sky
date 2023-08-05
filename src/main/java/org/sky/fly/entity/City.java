35f763ed9a725896d3941b5dfa4d91cfa471bae7
skyclouds2001 <skyclouds2001@163.com>
2023-08-05 17:13:08 +0800

    feat: support for validate

--- src/main/java/org/sky/fly/entity/City.java
+++ src/main/java/org/sky/fly/entity/City.java
@@ -2,6 +2,9 @@ package org.sky.fly.entity;
 
 import com.baomidou.mybatisplus.annotation.*;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -26,18 +29,23 @@ public class City implements Serializable {
 
     @Schema(description = "城市名称", requiredMode = Schema.RequiredMode.REQUIRED, accessMode = Schema.AccessMode.READ_WRITE)
     @TableField("name")
+    @NotBlank(message = "city name is required")
     private String name;
 
     @Schema(description = "城市所属地区名称", requiredMode = Schema.RequiredMode.REQUIRED, accessMode = Schema.AccessMode.READ_WRITE)
     @TableField("district")
+    @NotBlank(message = "city district is required")
     private String district;
 
     @Schema(description = "城市所属国家名称", requiredMode = Schema.RequiredMode.REQUIRED, accessMode = Schema.AccessMode.READ_WRITE)
     @TableField("country")
+    @NotBlank(message = "city country is required")
     private String country;
 
     @Schema(description = "城市人口", requiredMode = Schema.RequiredMode.REQUIRED, accessMode = Schema.AccessMode.READ_WRITE)
     @TableField("population")
+    @Min(value = 0, message = "city population must be an positive integer")
+    @Max(value = Integer.MAX_VALUE, message = "city population must not be an too big integer")
     private Integer population;
 
     @TableField(value="create_time", fill = FieldFill.INSERT)
